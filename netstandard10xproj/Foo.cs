b01a4ebc0962672e1f83730b5598b441397ebfac
Adam Ralph <adam@adamralph.com>
2016-11-15 13:16:40 +0100

    generalise

---
+++ netstandard10xproj/Foo.cs
@@ -0,0 +1,18 @@
+﻿#if NET45CSPROJ
+namespace net45csproj
+#elif NET45XPROJ
+namespace net45xproj
+#elif NETSTANDARD10CSPROJ
+namespace netstandard10csproj
+#elif NETSTANDARD10XPROJ
+namespace netstandard10xproj
+#endif
+{
+    public class Foo
+    {
+        public static void Bar(string baz)
+        {
+            // TODO: add some code which smoke tests compilation against your package
+        }
+    }
+}
